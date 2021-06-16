3b3fa8256261dca8baf73721c141faaf9174c5b1
elfizzy <abulkhair0800@gmail.com>
2021-06-16 17:03:44 +0100

    Updates

---
+++ precache-manifest.2db350f5abee124d09529f21ba2db98f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9d7b1ee79f5d1d3617b4bc020b63dba",
+    "url": "/Yin-Yen-Engineers/index.html"
+  },
+  {
+    "revision": "cc1e481403271ce5872e",
+    "url": "/Yin-Yen-Engineers/static/css/main.c1493406.chunk.css"
+  },
+  {
+    "revision": "cace7c80df5f6b90f5e4",
+    "url": "/Yin-Yen-Engineers/static/js/2.cce4633a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Yin-Yen-Engineers/static/js/2.cce4633a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc1e481403271ce5872e",
+    "url": "/Yin-Yen-Engineers/static/js/main.363392bc.chunk.js"
+  },
+  {
+    "revision": "1c99eed669941254cb67",
+    "url": "/Yin-Yen-Engineers/static/js/runtime-main.c80e0738.js"
+  },
+  {
+    "revision": "7ee60f881affabf1affcb867e2fd8a40",
+    "url": "/Yin-Yen-Engineers/static/media/building2.7ee60f88.jpeg"
+  },
+  {
+    "revision": "51c5743e67cc215f8ade2196199afe4d",
+    "url": "/Yin-Yen-Engineers/static/media/building3.51c5743e.jpeg"
+  },
+  {
+    "revision": "82e7e65002afd5c1e8da7301bd1960e4",
+    "url": "/Yin-Yen-Engineers/static/media/building4.82e7e650.jpeg"
+  },
+  {
+    "revision": "586d059deaf7f0cd0868f33433968e3d",
+    "url": "/Yin-Yen-Engineers/static/media/building5.586d059d.jpeg"
+  },
+  {
+    "revision": "d001b93614eef5801fe8733b0cb17302",
+    "url": "/Yin-Yen-Engineers/static/media/building6.d001b936.jpeg"
+  }
+]);
